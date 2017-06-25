65a8faad4568531b50406423871086bec385184e
PatriciaFM <patriciafm4@gmail.com>
2017-06-25 12:28:09 +0200

    Update Game class and Board mock added.

--- src/GOL/GameBundle/Domain/Game.php
+++ src/GOL/GameBundle/Domain/Game.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: patri
- * Date: 24-6-17
- * Time: 21:32
- */
+
+declare(strict_types=1);
 
 namespace GOL\GameBundle\Domain;
 
-
+/**
+ * Class Game
+ * @package GOL\GameBundle\Domain
+ */
 class Game
 {
+    /** @var null|Board */
+    private $board = null;
 
+    /**
+     * Game constructor.
+     * @param Board $board
+     */
+    public function __construct(Board $board)
+    {
+        $this->board = $board;
+    }
+
+    /**
+     * @return null|Board
+     */
+    public function getBoard()
+    {
+        return $this->board;
+    }
 }
--- src/GOL/GameBundle/Tests/Unit/Domain/GameTest.php
+++ src/GOL/GameBundle/Tests/Unit/Domain/GameTest.php
@@ -1,15 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: patri
- * Date: 24-6-17
- * Time: 21:34
- */
+
+declare(strict_types=1);
 
 namespace GOL\GameBundle\Test\Unit\Domain;
 
+use GOL\GameBundle\Domain\Board;
+use GOL\GameBundle\Domain\Game;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
 
-class GameTest
+class GameTest extends TestCase
 {
+    /** @var PHPUnit_Framework_MockObject_MockObject|Board */
+    private $boardMock = null;
 
+    public function setup()
+    {
+        $this->boardMock = $this->getMockBuilder(Board::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getStatus'])
+            ->getMock();
+    }
+
+    public function testStartGameShouldReturnAnEmptyBoard()
+    {
+        $this->boardMock->expects($this->once())
+            ->method('getStatus')
+            ->willReturn([['', '', '', ''], ['', '', '', ''], ['', '', '', '']]);
+
+        $game = new Game($this->boardMock);
+
+        $this->assertEquals([['', '', '', ''], ['', '', '', ''], ['', '', '', '']], $game->getBoard()->getStatus());
+    }
 }
