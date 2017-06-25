90f91ec19cccc2c4e1550efe870efc1cb2caa758
PatriciaFM <patriciafm4@gmail.com>
2017-06-25 12:25:42 +0200

    Added Game class and its unit test

---
+++ src/GOL/GameBundle/Domain/Game.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patri
+ * Date: 24-6-17
+ * Time: 21:32
+ */
+
+namespace GOL\GameBundle\Domain;
+
+
+class Game
+{
+
+}
---
+++ src/GOL/GameBundle/Tests/Unit/Domain/GameTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patri
+ * Date: 24-6-17
+ * Time: 21:34
+ */
+
+namespace GOL\GameBundle\Test\Unit\Domain;
+
+
+class GameTest
+{
+
+}
